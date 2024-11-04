82b4d4097907e2b183fbf2dd7f1a5ede1cdfd4b5
Ian Vaughn <ianbvaughn@gmail.com>
2024-11-04 15:34:35 -0800

    Fun with strings!

--- Objects.py
+++ Objects.py
@@ -33,3 +33,17 @@ tesseract = Box(8, 8, 8)
 print(tesseract.volume())
 print(tesseract.surface_area())
 print(tesseract._length)
+
+class Point:
+    """Represents a point in 2D space"""
+
+    def __init__(self,x,y):
+        self._xcoord = x
+        self._ycoord = y
+
+    def x_halved(self):
+        return self._xcoord / 2
+
+a = Point(30,150)
+b = a.x_halved()
+print(b)
--- Scratchpad.py
+++ Scratchpad.py
@@ -1,13 +1,1 @@
-class Point:
-    """Represents a point in 2D space"""
-
-    def __init__(self,x,y):
-        self._xcoord = x
-        self._ycoord = y
-
-    def x_halved(self):
-        return self._xcoord / 2
-
-a = Point(30,150)
-b = a.x_halved()
-print(b)
+print("char" > "ciarts")
