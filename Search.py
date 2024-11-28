96a8807a5cf75d158fc21daca3a43778844fc52d
Ian Vaughn <ianbvaughn@gmail.com>
2024-11-28 14:10:47 -0800

    Changed Search File

--- Search.py
+++ Search.py
@@ -1,4 +1,8 @@
 from time import time
+import math
+
+my_arr = [i for i in range (10000)]
+
 def binary_search(arr,target,low,high):
     mid=(low+high)//2
     if arr[mid]==target:
@@ -7,17 +11,28 @@ def binary_search(arr,target,low,high):
         return binary_search(arr,target,low,mid-1)
     elif target>arr[mid]:
         return binary_search(arr,target,mid+1,high)
+
 def linear_search(arr,target):
     for i in arr:
         if i==target:
             return i
     return -1
-my_arr = [i for i in range (10000)]
-print(time())
-index = binary_search(my_arr,5000,0,len(my_arr)-1)
+
+def jump_search(arr,target):
+    n=len(arr)
+    m=int(math.sqrt(n))
+    prev,curr=0,0
+    while curr<=n and arr[curr]<=target:
+        prev=curr
+        curr+=m
+    for i in range(prev,curr):
+        if arr[i]==target:
+            return i
+    return -1
+
+index = binary_search(my_arr,1000,0,len(my_arr)-1)
+index2 = linear_search(my_arr,1000)
+index3 = jump_search(my_arr,1000)
 print(index)
-print(time())
-index2 = linear_search(my_arr,10000)
-end2 = time()
 print(index2)
-print(time())
+print(index3)
